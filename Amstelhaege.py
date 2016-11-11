d500c8ca07e6ea391010b6634a2d664cebcaba07
JelleMul <muljelle@gmail.com>
2016-11-11 14:43:38 +0100

    update Amstelhaege met vrijstand

--- Amstelhaege.py
+++ Amstelhaege.py
@@ -21,6 +21,15 @@ class ConstructionSite(object):
         self.height = height
         # initializes an 2d array
         self.area = numpy.zeros((height, width), dtype='int32')
+    def buildVrijstand(self, x_start, x_end, y_start, y_end, std_vrijstand):
+        """
+        Build the eensgezinswoning on the given location
+        """
+        # change values in range
+        for x in range(x_start - std_vrijstand, x_end + std_vrijstand):
+            for y in range(y_start - std_vrijstand, y_end + std_vrijstand):
+                self.area[(y, x)] = 4
+
     def buildWoning(self, x_start, x_end, y_start, y_end, type):
         """
         Build the eensgezinswoning on the given location
@@ -29,15 +38,17 @@ class ConstructionSite(object):
         for x in range(x_start, x_end):
             for y in range(y_start, y_end):
                 self.area[(y, x)] = type
-    def checkIfPossible(self, x_start, x_end, y_start, y_end):
+
+    def checkIfPossible(self, x_start, x_end, y_start, y_end, std_vrijstand):
         """
         Check if it is possible to build on the given location
         """
-        for x in range(x_start, x_end):
-            for y in range(y_start, y_end):
-                if self.area[(y, x)] != 0:
+        for x in range(x_start - std_vrijstand, x_end + std_vrijstand):
+            for y in range(y_start - std_vrijstand, y_end + std_vrijstand):
+                if self.area[(y, x)] != 0 and self.area[(y, x)] != 4:
                     return False
         return True
+
 
 class Eensgezinswoning(object):
     """
@@ -53,40 +64,43 @@ def RunSimulation(mais, bung, egws, width, height):
     """
     area = ConstructionSite(width, height)
 
-    # build right amount of maisons
+
+    # build right amount of egws
     counter = 0
-    while counter < mais:
-        x_pos = random.randint(0, width - 20)
-        y_pos = random.randint(0, height - 15)
-        if area.checkIfPossible(x_pos, x_pos + 20, y_pos, y_pos + 15) == True:
-            area.buildWoning(x_pos, x_pos + 20, y_pos, y_pos + 15, 1)
+    while counter < egws:
+        x_pos = random.randint(0 + 4, width - 16 - 8)
+        y_pos = random.randint(0 + 4, height - 16 -8)
+        if area.checkIfPossible(x_pos, x_pos + 16, y_pos, y_pos + 16, 4) == True:
+            area.buildVrijstand(x_pos, x_pos + 16, y_pos, y_pos + 16, 4)
+            area.buildWoning(x_pos, x_pos + 16, y_pos, y_pos + 16, 1)
             counter += 1
 
     # build right amount of bungalows
     counter = 0
     while counter < bung:
-        x_pos = random.randint(0, width - 22)
-        y_pos = random.randint(0, height - 21)
-        if area.checkIfPossible(x_pos, x_pos + 22, y_pos, y_pos + 21) == True:
-            area.buildWoning(x_pos, x_pos + 22, y_pos, y_pos + 21, 2)
+        x_pos = random.randint(0 + 6, width - 20 - 12)
+        y_pos = random.randint(0 + 6, height - 15 - 12)
+        if area.checkIfPossible(x_pos, x_pos + 20, y_pos, y_pos + 15, 6) == True:
+            area.buildVrijstand(x_pos, x_pos + 20, y_pos, y_pos + 15, 6)
+            area.buildWoning(x_pos, x_pos + 20, y_pos, y_pos + 15, 2)
             counter += 1
 
-    # build right amount of egws
+    # build right amount of maisons
     counter = 0
-    while counter < egws:
-        x_pos = random.randint(0, width - 16)
-        y_pos = random.randint(0, height - 16)
-        if area.checkIfPossible(x_pos, x_pos + 16, y_pos, y_pos + 16) == True:
-            area.buildWoning(x_pos, x_pos + 16, y_pos, y_pos + 16, 3)
+    while counter < mais:
+        x_pos = random.randint(0 + 12, width - 22 - 24)
+        y_pos = random.randint(0 + 12, height - 21 - 24)
+        if area.checkIfPossible(x_pos, x_pos + 22, y_pos, y_pos + 21, 12) == True:
+            area.buildVrijstand(x_pos, x_pos + 22, y_pos, y_pos + 21, 12)
+            area.buildWoning(x_pos, x_pos + 22, y_pos, y_pos + 21, 3)
             counter += 1
+
+
+
     plt.imshow(area.area)
     #plt.gray()
     plt.show()
 
 
 
-RunSimulation(4, 5, 6, 300, 320)
-
-plt.imshow(area.area)
-#plt.gray()
-plt.show()
+RunSimulation(6, 10, 18 , 300, 320)
