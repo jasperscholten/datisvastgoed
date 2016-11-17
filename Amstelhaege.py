dd3c07182726065d5b0bff125a2e44ae68b97f43
JelleMul <muljelle@gmail.com>
2016-11-17 13:03:24 +0100

    bug removed, where some houses overlaped(removed the vrijstand)

--- Amstelhaege.py
+++ Amstelhaege.py
@@ -39,13 +39,13 @@ class ConstructionSite(object):
             for y in range(y_start, y_end):
                 self.area[(y, x)] = type
 
-    def checkIfPossible(self, x_start, x_end, y_start, y_end, std_vrijstand):
+    def checkIfPossible(self, x_start, x_end, y_start, y_end):
         """
         Check if it is possible to build on the given location
         """
-        for x in range(x_start - std_vrijstand, x_end + std_vrijstand):
-            for y in range(y_start - std_vrijstand, y_end + std_vrijstand):
-                if self.area[(y, x)] != 0 and self.area[(y, x)] != 4:
+        for x in range(x_start, x_end):
+            for y in range(y_start, y_end):
+                if self.area[(y, x)] != 0:
                     return False
         return True
 
@@ -60,7 +60,7 @@ def RunSimulation(mais, bung, egws, width, height):
     while counter < mais:
         x_pos = random.randint(0 + 12, width - 22 - 12)
         y_pos = random.randint(0 + 12, height - 21 - 12)
-        if area.checkIfPossible(x_pos, x_pos + 22, y_pos, y_pos + 21, 12) == True:
+        if area.checkIfPossible(x_pos, x_pos + 22, y_pos, y_pos + 21) == True:
             area.buildVrijstand(x_pos, x_pos + 22, y_pos, y_pos + 21, 12)
             area.buildWoning(x_pos, x_pos + 22, y_pos, y_pos + 21, 3)
             counter += 1
@@ -70,7 +70,7 @@ def RunSimulation(mais, bung, egws, width, height):
     while counter < bung:
         x_pos = random.randint(0 + 6, width - 20 - 6)
         y_pos = random.randint(0 + 6, height - 15 - 6)
-        if area.checkIfPossible(x_pos, x_pos + 20, y_pos, y_pos + 15, 6) == True:
+        if area.checkIfPossible(x_pos, x_pos + 20, y_pos, y_pos + 15) == True:
             area.buildVrijstand(x_pos, x_pos + 20, y_pos, y_pos + 15, 6)
             area.buildWoning(x_pos, x_pos + 20, y_pos, y_pos + 15, 2)
             counter += 1
@@ -79,8 +79,8 @@ def RunSimulation(mais, bung, egws, width, height):
     counter = 0
     while counter < egws:
         x_pos = random.randint(0 + 4, width - 16 - 4)
-        y_pos = random.randint(0 + 4, height - 16 -4)
-        if area.checkIfPossible(x_pos, x_pos + 16, y_pos, y_pos + 16, 4) == True:
+        y_pos = random.randint(0 + 4, height - 16 - 4)
+        if area.checkIfPossible(x_pos, x_pos + 16, y_pos, y_pos + 16) == True:
             area.buildVrijstand(x_pos, x_pos + 16, y_pos, y_pos + 16, 4)
             area.buildWoning(x_pos, x_pos + 16, y_pos, y_pos + 16, 1)
             counter += 1
